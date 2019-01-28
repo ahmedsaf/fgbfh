e49ae5cbc3714cb638b9b75f6be76674bb69d559
ahmedsaf <45786631+ahmedsaf@users.noreply.github.com>
2019-01-28 23:43:00 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("539558344827076610")
+setInterval(function() {
+channel.send(`تبال[أyhh<uyumnffإGSJHDGSJGJDSKNGSDHJKGNJKDHTKRJتىالاتيالايالفلارفساتئةرسVJHCXFURGHYUZSBCUZDYIKVCFYHسرتاالتتابصفقتلاافىتةىتىنئبستشFXZNMلالاببتاسرىGJGJHUHJIGJGUHJFYSHFHFGBMBKGJHGMGBJBNBJHMHIHJHI9HUHJHUYJHJHDGFYFGTSFDRDFSRSEGDDFMBMBNHKJHFKJHGIUHHJYUHIGKFJHX;LDJDLH;JKHDY`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);"NTM1NDMzNjM4Nzg4MDcxNDMz.DzEF6A.-G_7M1NDgAOVD_UxQyZCxI3sUBE"
